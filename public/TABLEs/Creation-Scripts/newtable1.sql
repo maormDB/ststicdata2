ab7164c7bd767ca060958c81c5eba1f229cf3afe
maorm@dbmaestro.com <maorm@dbmaestro.com>
2026-07-16 18:32:49 +0300

    test1

---
+++ public/TABLEs/Creation-Scripts/newtable1.sql
@@ -0,0 +1,4 @@
+CREATE TABLE public.newtable1 (
+    column1 character varying,
+    column2 character varying
+);
---
+++ public/TABLEs/Creation-Scripts/orders.sql
@@ -0,0 +1,10 @@
+CREATE TABLE public.orders (
+    id integer NOT NULL,
+    customer character varying(100),
+    status public.order_status,
+    amount numeric(12,2)
+);
+
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_pkey PRIMARY KEY (id);
